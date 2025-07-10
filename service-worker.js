e807d97adff4464906f5f72e17d7d9ab40a316ad
Light <lliu.llight@gmail.com>
2025-07-10 11:48:35 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -13,9 +13,10 @@ const STATIC_FILES = [
   '/app.js',
   '/playlists.json',
   '/cmm-single-build-player/libs/aframe-v1.7.1.min.js',
+  '/cmm-single-build-player/libs/aframe.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
-  '/libs/aframe.min.js',
-  '/libs/aframe-v1.7.1.min.js'
+  // '/libs/aframe.min.js',
+  // '/libs/aframe-v1.7.1.min.js'
 ];
 
 // Install event - cache static files
